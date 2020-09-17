3b26c6eae3b2b87955d900c6e13b608a374d847d
Korolev Dmitriy <kekskerd@gmail.com>
2020-09-17 21:53:05 +0300

    task 3 done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles/main.css';
 
 import { foo, createCb } from './asynchronous-programming/task1';
 import { parseJSON, succesCb, failureCb } from './asynchronous-programming/task2';
-
+import { delay } from './asynchronous-programming/task3';
 //    TASK 1
 console.log('======TASK 1=====');
 foo(5, createCb('cb'));
@@ -11,3 +11,6 @@ foo(20, createCb('cb'));
 console.log('======TASK 2=====');
 parseJSON('{"x": 10}', succesCb, failureCb);
 parseJSON('{x}', succesCb, failureCb);
+
+console.log('======TASK 3=====');
+delay(1500).then(value => console.log(`Done with ${value}`));
